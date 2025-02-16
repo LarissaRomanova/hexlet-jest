ec2e21c6a086e3e0296db5eaf413a601542d8469
Larisa Romanova <larissa.kstu@gmail.com>
2025-02-16 14:00:15 +0300

    add testing of reverse() function with long text

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,25 @@
 import reverse from '../src/index.js';
 
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
+});
+
+test('reverse', () => {
+  const text = readFile('sourse.txt');
+  const result = readFile('result.txt');
+
+  expect(reverse(text)).toEqual(result);
 });
